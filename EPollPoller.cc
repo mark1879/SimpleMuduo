af0ff938fbfc11e30764f979972992f8bc801cd2
mark1879 <mark1879@163.com>
2023-03-30 15:06:34 +0800

    optimized EPollPoller

--- EPollPoller.cc
+++ EPollPoller.cc
@@ -27,13 +27,13 @@ Timestamp EPollPoller::Poll(int timeout_ms, ChannelList *activate_channel_list)
     LOG_INFO("%s: total fd count: %lu\n", __FUNCTION__, channel_map_.size());
 
     int events_num = ::epoll_wait(epoll_fd_, &*event_list_.begin(), static_cast<int>(event_list_.size()), timeout_ms);
-    int saved_errno = errno;
+    int saved_errno = errno;    // Prevents errno from being overwritten
     Timestamp now(Timestamp::now());
 
     if (events_num > 0)
     {
         LOG_INFO("%s: %d events triggered\n", __FUNCTION__, events_num);
-        FillActivateChannels(events_num, activate_channel_list);
+        FillActivateChannels(events_num, activate_channel_list); 
         if (events_num == event_list_.size())
         {
             event_list_.resize(event_list_.size() * 2);
@@ -47,7 +47,7 @@ Timestamp EPollPoller::Poll(int timeout_ms, ChannelList *activate_channel_list)
     {
         if (saved_errno != EINTR)
         {
-            errno = saved_errno;
+            errno = saved_errno ;
             LOG_ERROR("%s: error!", __FUNCTION__);
         }
     }
